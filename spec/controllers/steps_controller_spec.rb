b1009f9195212a936cfa06d976172a62f4fd8011
Laura Brooks <laura.brooks0@outlook.com>
2025-11-10 18:30:04 +0900

    add spec

---
+++ spec/controllers/steps_controller_spec.rb
@@ -0,0 +1,8 @@
+Rails.describe StepsController, type: :controller do
+  describe "GET #index" do
+    it "returns a success response" do
+      get :index
+      expect(response).to be_successful
+    end
+  end
+end
